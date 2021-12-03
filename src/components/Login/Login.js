d9a2ddf2445ad013a947e97bd58edde73ace08ab
Md Sakibul Islam <86571674+Md-Sakibul-Islam@users.noreply.github.com>
2021-12-03 19:05:19 +0600

    login ui done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,34 @@
-import React, { useContext } from 'react';
-
-
+import React, { useContext } from "react";
+import logo from "../../images/login.png";
+import './Login.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login</h1>
+  return (
+    <div className="container">
+      <h4 className="mt-5">
+        Please <span className="text-danger">Login Here</span>
+      </h4>
+      <div className="row">
+        <div className="col-md-6 col-lg-6 col-12">
+          <img src={logo} alt="" />
         </div>
-    );
+        <div className="col-md-6 col-lg-6 col-12">
+          <div className="login-from">
+          <input type="email" name="" id="" placeholder="Your Email" />
+          <br />
+          <br />
+          <input type="password" name="" id="" placeholder="Your Password" />
+          <br /><br />
+          <div>
+          <button className="btn btn-danger login-btn">Login</button>
+          <button className="btn btn-danger login-btn">Google</button>
+          </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
